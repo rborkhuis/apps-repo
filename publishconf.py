5b042e0162e15156119c5b40ef1f9869e35cb8d1
rick borkhuis <>
2022-12-06 04:14:36 +0100

    small config changes

--- publishconf.py
+++ publishconf.py
@@ -11,12 +11,12 @@ from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
 SITEURL = 'https://rborkhuis.github.io/apps-repo/'
-RELATIVE_URLS = True
+RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
-DELETE_OUTPUT_DIRECTORY = True
+DELETE_OUTPUT_DIRECTORY = False
 
 # Following items are often useful when publishing
 
